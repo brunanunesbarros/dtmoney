f6d766ff2a8da0a49ba9b3e267c4bba5e44c3e0f
Bruna Barros <brunamary99@gmail.com>
2021-11-12 10:50:26 -0300

    criar e usar context api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,8 @@ import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { FormEvent, useState } from "react";
-import { api } from "../../services/api";
+import { FormEvent, useContext, useState } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -13,24 +13,30 @@ interface NewTransactionModalProps {
 
 Modal.setAppElement("#root");
 
-export function NewTransactionModal({isOpen,onRequestClose}: NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     const [type, setType] = useState("deposit");
     const [title, setTitle] = useState("");
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState("");
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    const {createTransaction} = useContext(TransactionsContext)
+
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-        const data = {
-          title,
-          value,
-          category,
-          type
-        }
 
-        api.post('/transactions', data)
+        await createTransaction({
+            title,
+            amount,
+            category,
+            type
+        })
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
-
+       
     return (
         <Modal
             isOpen={isOpen}
@@ -58,8 +64,8 @@ export function NewTransactionModal({isOpen,onRequestClose}: NewTransactionModal
                 <input
                     placeholder="Valor"
                     type="number"
-                    value={value}
-                    onChange={(e) => setValue(Number(e.target.value))}
+                    value={amount}
+                    onChange={(e) => setAmount(Number(e.target.value))}
                 />
 
                 <TransactionTypeContainer>
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
-interface Transaction {
-  id: number,
-  title: string,
-  amount: number,
-  type: string,
-  category: string,
-  createdAt: string
-}
 
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  useEffect(() => {
-    api.get('transactions')
-      .then((response) => setTransactions(response.data.transactions))
-  },[])
-
+  const {transactions} = useContext(TransactionsContext);
+  console.log(transactions)
   return(
     <Container>
       <table>
@@ -35,7 +22,7 @@ export function TransactionsTable() {
             return(
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
-                <td className={transaction.type}>''
+                <td className={transaction.type}>
                   {new Intl.NumberFormat('pt-BR', {
                     style: 'currency',
                     currency: 'BRL'
