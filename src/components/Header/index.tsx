5823676dfcaa0d57451011163811b19028ea71fe
Bruna Barros <brunamary99@gmail.com>
2021-11-12 01:23:50 -0300

    primeiro commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+type HeaderProps = {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header(props: HeaderProps) {
+  
+  return(
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dtmoney" />
+        <button type="button" onClick={props.onOpenNewTransactionModal} >
+          Nova Transação
+        </button>
+        
+      </Content>
+    </Container>
+  )
+}
